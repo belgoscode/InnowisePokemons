55ad1fcc8cfccc89caa375229f906487a8adbf33
belgoscode <tima@darida.net>
2024-06-16 22:55:20 +0300

    Pokemons hardcoded, pictures added.

--- app/src/main/java/com/example/innowisepokemons/PokemonList.kt
+++ app/src/main/java/com/example/innowisepokemons/PokemonList.kt
@@ -4,21 +4,36 @@ object PokemonList {
     val pokemons: List<Pokemon> = listOf(
         Pokemon(
             id = 1,
-            name = "First",
+            name = "Bulbasaur",
             picture = R.drawable.bulbasaur,
-            type = "First type",
-            height = 0.0,
-            weight = 0.0
+            type = "Grass, Poison",
+            height = 0.7,
+            weight = 6.9
         ),
         Pokemon(
             id = 2,
-            name = "Second",
-            picture = R.drawable.bulbasaur,
-            type = "Second type",
-            height = 0.0,
-            weight = 0.0
+            name = "Ivysaur",
+            picture = R.drawable.ivysaur,
+            type = "Grass, Poison",
+            height = 1.0,
+            weight = 13.0
         ),
-
+        Pokemon(
+            id = 3,
+            name = "Charmander",
+            picture = R.drawable.charmander,
+            type = "Fire",
+            height = 0.6,
+            weight = 8.5
+        ),
+        Pokemon(
+            id = 4,
+            name = "Squirtle",
+            picture = R.drawable.squirtle,
+            type = "Water",
+            height = 0.5,
+            weight = 9.0
+        ),
         // Сюда хардкодить покемонов
     )
 
